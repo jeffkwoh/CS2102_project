8d70380fab94925618c58405feddf3a5c37c86d9
remo5000 <vigneshshankar@gmail.com>
2018-10-09 00:31:00 +0800

    Add routing for driver controller

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var db = require('./model/db');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var driverRouter = require('./routes/driver');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/driver', driverRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
