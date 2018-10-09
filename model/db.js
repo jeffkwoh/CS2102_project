3853fb1a87e8110fd5db7a8cb388e18a621bd4b2
remo5000 <vigneshshankar@gmail.com>
2018-10-09 12:18:08 +0800

    Add rest of init queries

--- model/db.js
+++ model/db.js
@@ -26,11 +26,48 @@ const initDb = () => {
       name VARCHAR(64) NOT NULL,
       password VARCHAR(32) NOT NULL
     );`, 'Users')
+
+  initTable(`
+    CREATE TABLE IF NOT EXISTS userOwnsACar (
+      licensePlate VARCHAR(10) PRIMARY KEY, 
+      owner INTEGER NOT NULL,
+      carBrand VARCHAR (64) NOT NULL, 
+      carModel VARCHAR (64) NOT NULL, 
+      numSeats INTEGER NOT NULL CHECK (numSeats > 0),
+      FOREIGN KEY (owner) REFERENCES appUserAccount(userID)
+    );`, 'Cars')
+
+  initTable(`
+    CREATE TABLE IF NOT EXISTS advertisedCarRide (
+      driver INTEGER REFERENCES appUserAccount(userID), 
+      car VARCHAR(10) REFERENCES userOwnsAcar(licensePlate), 
+      date DATE NOT NULL, 
+      time TIME NOT NULL, 
+      origin VARCHAR(128) NOT NULL, 
+      destination VARCHAR(128) NOT NULL, 
+      PRIMARY KEY (driver, date, time, origin, destination) 
+    );`, 'CarRide')
+
+  initTable(`
+    CREATE TABLE IF NOT EXISTS bid (
+      bidStatus CHAR(12) CHECK(bidStatus = 'pending' OR bidStatus ='unsuccessful' 
+        OR bidStatus = 'successful') NOT NULL, 
+      bidAmount DECIMAL(6,2) NOT NULL CHECK (bidAmount >= 0), 
+      bidder INTEGER NOT NULL, 
+      driver INTEGER NOT NULL,
+      date DATE NOT NULL,  
+      time TIME NOT NULL, 
+      origin VARCHAR(128), 
+      destination VARCHAR(128), 
+      FOREIGN KEY(bidder) REFERENCES appUserAccount(userID), 
+      FOREIGN KEY(driver, date, time, origin, destination)  
+      REFERENCES advertisedCarRide(driver, date, time, origin, destination), 
+      PRIMARY KEY(bidder, driver, date, time, origin, destination) 
+    );`, 'CarRide')
 }
 
-
 /* DB helpers to be exported */
-const user = require('user.js')
+const user = require('./user.js')
 module.exports = {
   initDb,
   user,
