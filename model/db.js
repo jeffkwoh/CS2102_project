5a5eefa28393cd0c8f42e17e9b36d3c7d6ee1326
remo5000 <vigneshshankar@gmail.com>
2018-10-09 11:36:07 +0800

    Add user import

--- model/db.js
+++ model/db.js
@@ -1,9 +1,8 @@
 const pgp = require('pg-promise')({
   // initialisation options.
-});
-const cn = require('../config/config.js');
-const db = pgp(cn);
-
+})
+const cn = require('../config/config.js')
+const db = pgp(cn)
 
 const initTable = (query, tableName) =>
   db.none(query)
@@ -15,32 +14,25 @@ const initTable = (query, tableName) =>
       // error;
       console.log(error);
     });
+
 /* Databse initialisation */
-// Users
-const initDb = () =>
-  db.none(`
+const initDb = () => {
+  // Users
+  initTable(`
     CREATE TABLE IF NOT EXISTS appUserAccount (
       userID SERIAL PRIMARY KEY,
       email VARCHAR(128) UNIQUE NOT NULL,
       contactNumber VARCHAR(15) NOT NULL,
       name VARCHAR(64) NOT NULL,
       password VARCHAR(32) NOT NULL
-    );`)
-    .then(() => {
-      console.log("Created Users table!");
-      // success;
-    })
-    .catch(error => {
-      console.log(error);
-      // error;
-    });
+    );`, 'Users')
+}
 
 
-
-
-
+/* DB helpers to be exported */
+const user = require('user.js')
 module.exports = {
-  addUser,
-  getAllUsers,
+  initDb,
+  user,
   exposedInstance : db
 };
