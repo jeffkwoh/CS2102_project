a37f053feb5ef7fbf903bb98ccf83615ef38fe3c
remo5000 <vigneshshankar@gmail.com>
2018-10-09 11:15:48 +0800

    Add user initialisation

--- model/db.js
+++ model/db.js
@@ -1,37 +1,60 @@
-
 const pgp = require('pg-promise')({
-// initialisation options.
+  // initialisation options.
 });
 
 const cn = require('../config/config.js');
 
+console.log(cn);
 const db = pgp(cn);
 
+
+/* Databse initialisation */
+// Users
+db.none(`
+  CREATE TABLE IF NOT EXISTS appUserAccount (
+    userID SERIAL PRIMARY KEY,
+    email VARCHAR(128) UNIQUE NOT NULL,
+    contactNumber VARCHAR(15) NOT NULL,
+    name VARCHAR(64) NOT NULL,
+    password VARCHAR(32) NOT NULL
+  );`)
+  .then(() => {
+    console.log("Created Users table!");
+    // success;
+  })
+  .catch(error => {
+    console.log(error);
+    // error;
+  });
+
+
+
+
 const addUser = (user_name) =>  {
-    db.none('INSERT INTO Users(name) VALUES($1)', [user_name])
-        .then(() => {
-            console.log("success!");
-            // success;
-        })
-        .catch(error => {
-            console.log(error);
-            // error;
-        });
+  db.none('INSERT INTO Users(name) VALUES($1)', [user_name])
+    .then(() => {
+      console.log("success!");
+      // success;
+    })
+    .catch(error => {
+      console.log(error);
+      // error;
+    });
 };
 
 const getAllUsers = () => {
-    db.any('SELECT * FROM Users')
-        .then(data => {
-            console.log(data); // print data;
-            return data;
-        })
-        .catch(error => {
-            console.log(error); // print the error;
-        })
+  db.any('SELECT * FROM Users')
+    .then(data => {
+      console.log(data); // print data;
+      return data;
+    })
+    .catch(error => {
+      console.log(error); // print the error;
+    })
 };
 
 module.exports = {
-    addUser,
-    getAllUsers,
-    exposedInstance : db
+  addUser,
+  getAllUsers,
+  exposedInstance : db
 };
