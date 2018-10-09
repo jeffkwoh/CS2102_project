eb13af378e2f7f2630b81a6928ed4e9be1f17e89
remo5000 <vigneshshankar@gmail.com>
2018-10-09 11:32:51 +0800

    Abstract initialisation function

--- model/db.js
+++ model/db.js
@@ -1,57 +1,43 @@
 const pgp = require('pg-promise')({
   // initialisation options.
 });
-
 const cn = require('../config/config.js');
-
-console.log(cn);
 const db = pgp(cn);
 
 
+const initTable = (query, tableName) =>
+  db.none(query)
+    .then(() => {
+      // success;
+      console.log(`Created ${tableName} table!`);
+    })
+    .catch(error => {
+      // error;
+      console.log(error);
+    });
 /* Databse initialisation */
 // Users
-db.none(`
-  CREATE TABLE IF NOT EXISTS appUserAccount (
-    userID SERIAL PRIMARY KEY,
-    email VARCHAR(128) UNIQUE NOT NULL,
-    contactNumber VARCHAR(15) NOT NULL,
-    name VARCHAR(64) NOT NULL,
-    password VARCHAR(32) NOT NULL
-  );`)
-  .then(() => {
-    console.log("Created Users table!");
-    // success;
-  })
-  .catch(error => {
-    console.log(error);
-    // error;
-  });
-
-
-
-
-const addUser = (user_name) =>  {
-  db.none('INSERT INTO Users(name) VALUES($1)', [user_name])
+const initDb = () =>
+  db.none(`
+    CREATE TABLE IF NOT EXISTS appUserAccount (
+      userID SERIAL PRIMARY KEY,
+      email VARCHAR(128) UNIQUE NOT NULL,
+      contactNumber VARCHAR(15) NOT NULL,
+      name VARCHAR(64) NOT NULL,
+      password VARCHAR(32) NOT NULL
+    );`)
     .then(() => {
-      console.log("success!");
+      console.log("Created Users table!");
       // success;
     })
     .catch(error => {
       console.log(error);
       // error;
     });
-};
 
-const getAllUsers = () => {
-  db.any('SELECT * FROM Users')
-    .then(data => {
-      console.log(data); // print data;
-      return data;
-    })
-    .catch(error => {
-      console.log(error); // print the error;
-    })
-};
+
+
+
 
 module.exports = {
   addUser,
