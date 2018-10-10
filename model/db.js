2dbe3fbd66d356f6c8e775979d12c85c0c6d978e
remo5000 <vigneshshankar@gmail.com>
2018-10-10 19:16:54 +0800

    Use a transaction for init

--- model/db.js
+++ model/db.js
@@ -11,7 +11,7 @@ const db = pgp(cn)
  * @param query{string} the query to be run
  * @param log{string} the log message to be displayed when query is successful
  */
-const runQuery = (query, log) => 
+const runQuery = (query, log) =>
   db.none(query)
     .then(() => {
       // success;
@@ -21,58 +21,60 @@ const runQuery = (query, log) =>
       // error;
       console.log(error);
     });
-const initTableQuery = (query, tableName) => runQuery(query, `Created ${tableName} table!`)
-const dropTableQuery = (query, tableName) => runQuery(query, `Dropped ${tableName} table!`)
 
-/* Databse initialisation */
+/**
+ * Initialises database, with all the necessary tables specified in the database
+ * design of this application.
+ */
 const initDb = () => {
-  // Users
-  initTableQuery(`
-    CREATE TABLE IF NOT EXISTS appUserAccount (
-      userID SERIAL PRIMARY KEY,
-      email VARCHAR(128) UNIQUE NOT NULL,
-      contactNumber VARCHAR(15) NOT NULL,
-      name VARCHAR(64) NOT NULL,
-      password VARCHAR(32) NOT NULL
-    );`, 'appUserAccount')
+  const query = `
+  BEGIN
 
-  initTableQuery(`
-    CREATE TABLE IF NOT EXISTS userOwnsACar (
-      licensePlate VARCHAR(10) PRIMARY KEY, 
-      owner INTEGER NOT NULL,
-      carBrand VARCHAR (64) NOT NULL, 
-      carModel VARCHAR (64) NOT NULL, 
-      numSeats INTEGER NOT NULL CHECK (numSeats > 0),
-      FOREIGN KEY (owner) REFERENCES appUserAccount(userID)
-    );`, 'userOwnsACar')
+  CREATE TABLE IF NOT EXISTS appUserAccount (
+    userID SERIAL PRIMARY KEY,
+    email VARCHAR(128) UNIQUE NOT NULL,
+    contactNumber VARCHAR(15) NOT NULL,
+    name VARCHAR(64) NOT NULL,
+    password VARCHAR(32) NOT NULL
+  );
 
-  initTableQuery(`
-    CREATE TABLE IF NOT EXISTS advertisedCarRide (
-      driver INTEGER REFERENCES appUserAccount(userID), 
-      car VARCHAR(10) REFERENCES userOwnsAcar(licensePlate), 
-      date DATE NOT NULL, 
-      time TIME NOT NULL, 
-      origin VARCHAR(128) NOT NULL, 
-      destination VARCHAR(128) NOT NULL, 
-      PRIMARY KEY (driver, date, time, origin, destination) 
-    );`, 'advertisedCarRide')
+  CREATE TABLE IF NOT EXISTS userOwnsACar (
+    licensePlate VARCHAR(10) PRIMARY KEY,
+    owner INTEGER NOT NULL,
+    carBrand VARCHAR (64) NOT NULL,
+    carModel VARCHAR (64) NOT NULL,
+    numSeats INTEGER NOT NULL CHECK (numSeats > 0),
+    FOREIGN KEY (owner) REFERENCES appUserAccount(userID)
+  );
 
-  initTableQuery(`
-    CREATE TABLE IF NOT EXISTS bid (
-      bidStatus CHAR(12) CHECK(bidStatus = 'pending' OR bidStatus ='unsuccessful' 
-        OR bidStatus = 'successful') NOT NULL, 
-      bidAmount DECIMAL(6,2) NOT NULL CHECK (bidAmount >= 0), 
-      bidder INTEGER NOT NULL, 
-      driver INTEGER NOT NULL,
-      date DATE NOT NULL,  
-      time TIME NOT NULL, 
-      origin VARCHAR(128), 
-      destination VARCHAR(128), 
-      FOREIGN KEY(bidder) REFERENCES appUserAccount(userID), 
-      FOREIGN KEY(driver, date, time, origin, destination)  
-      REFERENCES advertisedCarRide(driver, date, time, origin, destination), 
-      PRIMARY KEY(bidder, driver, date, time, origin, destination) 
-    );`, 'bid')
+  CREATE TABLE IF NOT EXISTS advertisedCarRide (
+    driver INTEGER REFERENCES appUserAccount(userID),
+    car VARCHAR(10) REFERENCES userOwnsAcar(licensePlate),
+    date DATE NOT NULL,
+    time TIME NOT NULL,
+    origin VARCHAR(128) NOT NULL,
+    destination VARCHAR(128) NOT NULL,
+    PRIMARY KEY (driver, date, time, origin, destination)
+  );
+
+  CREATE TABLE IF NOT EXISTS bid (
+    bidStatus CHAR(12) CHECK(bidStatus = 'pending' OR bidStatus ='unsuccessful'
+      OR bidStatus = 'successful') NOT NULL,
+    bidAmount DECIMAL(6,2) NOT NULL CHECK (bidAmount >= 0),
+    bidder INTEGER NOT NULL,
+    driver INTEGER NOT NULL,
+    date DATE NOT NULL,
+    time TIME NOT NULL,
+    origin VARCHAR(128),
+    destination VARCHAR(128),
+    FOREIGN KEY(bidder) REFERENCES appUserAccount(userID),
+    FOREIGN KEY(driver, date, time, origin, destination)
+    REFERENCES advertisedCarRide(driver, date, time, origin, destination),
+    PRIMARY KEY(bidder, driver, date, time, origin, destination)
+  );
+  COMMIT;`
+
+  runQuery(query, 'Initialised all tables!')
 }
 
 const deinitDb = () => {
