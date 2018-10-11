ea8a485304d64e860bcff8682c6ded6ecfc3ce2f
chikchengyao <chikchengyao@gmail.com>
2018-10-12 01:26:23 +0800

    Change model/bid.js to use block SQL strings

--- model/bid.js
+++ model/bid.js
@@ -1,7 +1,19 @@
 // Creates a bid for a user
 const createUserBid = (user, bidAmount, driver, date, time, origin, destination, db) =>  {
-    db.none('INSERT INTO bid(bidder, bidStatus, bidAmount, driver, date, time, origin, destination) ' +
-        'VALUES($1, $2, $3, $4, $5, $6, $7, $8)', [user, 'pending', bidAmount, driver, date, time, origin, destination])
+    db.none(
+        `
+        INSERT INTO bid(bidder, bidStatus, bidAmount, driver, date, time, origin, destination) 
+        VALUES(
+            ${user}, 
+            'pending', 
+            ${bidAmount}, 
+            ${driver}, 
+            ${date}, 
+            ${time}, 
+            ${origin}, 
+            ${destination}
+        );
+        `)
         .then(() => {
             console.log("success!");
             // success;
@@ -14,9 +26,17 @@ const createUserBid = (user, bidAmount, driver, date, time, origin, destination,
 
 // Updates the bid of a User
 const updateUserBid = (user, bidAmount, driver, date, time, origin, destination, db) =>  {
-    db.none('UPDATE bid SET b.bidAmount = $1 FROM bid b WHERE b.bidder = $2 ' +
-        'AND b.driver = $3 AND b.date = $4 AND b.time = $5 AND b.origin = $6 AND b.destination = $7, ', +
-        [bidAmount, user, driver, date, time, origin, destination])
+    db.none(`
+        UPDATE bid 
+        SET b.bidAmount = ${bidAmount} 
+        FROM bid b 
+        WHERE b.bidder = ${user}
+        AND b.driver = ${driver}
+        AND b.date = ${date}
+        AND b.time = ${time}
+        AND b.origin = ${origin}
+        AND b.destination = ${destination};
+        `)
         .then(() => {
             console.log("success!");
             // success;
@@ -29,8 +49,14 @@ const updateUserBid = (user, bidAmount, driver, date, time, origin, destination,
 
 // Deletes the bid of a User
 const deleteUserBid = (user, driver, date, time, origin, destination, db) =>  {
-    db.none('DELETE FROM bid b WHERE b.bidder = $1 AND b.driver = $2 AND b.date = $3 ' +
-        'AND b.time = $4 AND b.origin = $5 AND b.destination = $6 ', [user, driver, date, time, origin, destination])
+    db.none(`
+        DELETE FROM bid b 
+        WHERE b.bidder = ${user} 
+        AND b.driver = ${driver} 
+        AND b.date = ${date} 
+        AND b.time = ${time} 
+        AND b.origin = ${origin} 
+        AND b.destination = ${destination};`)
         .then(() => {
             console.log("success!");
             // success;
@@ -43,7 +69,9 @@ const deleteUserBid = (user, driver, date, time, origin, destination, db) =>  {
 
 // List all bids a user has made
 const listBidsAUserHas = (user, db) =>  {
-    db.any('SELECT b.date, b.time, b.origin, b.destination, b.bidAmount FROM bid b WHERE b.bidder = $1', [user])
+    db.any(`
+        SELECT b.date, b.time, b.origin, b.destination, b.bidAmount 
+        FROM bid b WHERE b.bidder = ${user};`)
         .then(() => {
             console.log("success!");
             // success;
@@ -56,9 +84,16 @@ const listBidsAUserHas = (user, db) =>  {
 
 // Updates an existing bid in the table Bid as successful
 const setBidAsSuccessful = (successfulBidder, driver, date, time, origin, destination, db) =>  {
-    db.none('UPDATE bid SET b.bidStatus = $1 FROM bid b WHERE b.bidder = $2 ' +
-        'AND b.driver = $3 AND b.date = $4 AND b.time = $5 AND b.origin = $6 AND b.destination = $7, ', +
-        ['successful', successfulBidder, driver, date, time, origin, destination])
+    db.none(`
+        UPDATE bid 
+        SET b.bidStatus = ${successful} 
+        FROM bid b 
+        WHERE b.bidder = ${successfulBidder} 
+        AND b.driver = ${driver} 
+        AND b.date = ${date} 
+        AND b.time = ${time} 
+        AND b.origin = ${origin} 
+        AND b.destination = ${destination};`)
         .then(() => {
             console.log("success!");
             // success;
@@ -71,9 +106,16 @@ const setBidAsSuccessful = (successfulBidder, driver, date, time, origin, destin
 
 // Updates an existing bid in the table Bid as unsuccessful
 const setBidAsUnsuccessful = (unsuccessfulBidder, driver, date, time, origin, destination, db) =>  {
-    db.none('UPDATE bid SET b.bidStatus = $1 FROM bid b WHERE b.bidder = $2 ' +
-        'AND b.driver = $3 AND b.date = $4 AND b.time = $5 AND b.origin = $6 AND b.destination = $7, ', +
-        ['unsuccessful', unsuccessfulBidder, driver, date, time, origin, destination])
+    db.none(`
+        UPDATE bid 
+        SET b.bidStatus = 'unsuccessful' 
+        FROM bid b 
+        WHERE b.bidder = ${unsuccessfulBidder} 
+        AND b.driver = ${driver} 
+        AND b.date = ${date} 
+        AND b.time = ${time} 
+        AND b.origin = ${origin}
+        AND b.destination = ${destination};`)
         .then(() => {
             console.log("success!");
             // success;
