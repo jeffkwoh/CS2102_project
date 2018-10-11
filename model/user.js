103b40685239bbd9f972a13dc100759224dbd6dd
chikchengyao <chikchengyao@gmail.com>
2018-10-12 01:10:53 +0800

    Format block SQL strings properly

--- model/user.js
+++ model/user.js
@@ -1,7 +1,13 @@
 // Allows creation of usr account
 const createUserAppAccount = (userEmail, userContactNumber, userName, userPassword, db) =>  {
-    db.none(`INSERT INTO appUserAccount(email, contactNumber, name, password) ' +
-        'VALUES(${userEmail}, ${userContactNumber}, ${userName}, ${userPassword}`)
+    db.none(`
+        INSERT INTO appUserAccount(email, contactNumber, name, password) 
+        VALUES(
+            ${userEmail}, 
+            ${userContactNumber}, 
+            ${userName}, 
+            ${userPassword}
+        );`)
         .then(() => {
             console.log("success!");
             // success;
@@ -14,7 +20,9 @@ const createUserAppAccount = (userEmail, userContactNumber, userName, userPasswo
 
 // Lists out the names and emails of everyone in userAppAccounts
 const listUserAppAccount = (db) =>  {
-    db.any('SELECT a.name, a.email FROM appUserAccount a')
+    db.any(`
+        SELECT a.name, a.email 
+        FROM appUserAccount a;`)
         .then(() => {
             console.log("success!");
             // success;
@@ -27,8 +35,16 @@ const listUserAppAccount = (db) =>  {
 
 // Adds a car to a specific user
 const addCarToUser = (owner, licensePlate, carBrand, carModel, numSeats, db) =>  {
-    db.none(`INSERT INTO userOwnsACar(licensePlate, owner, carBrand, carModel, numSeats) ' +
-        'VALUES(${licensePlate}, ${owner}, ${carBrand}, ${carModel}, ${numSeats})`)
+    db.none(`
+        INSERT INTO userOwnsACar(licensePlate, owner, carBrand, carModel, numSeats) 
+        VALUES(
+            ${licensePlate}, 
+            ${owner}, 
+            ${carBrand}, 
+            ${carModel}, 
+            ${numSeats}
+        );`
+        )
         .then(() => {
             console.log("success!");
             // success;
@@ -42,7 +58,11 @@ const addCarToUser = (owner, licensePlate, carBrand, carModel, numSeats, db) => 
 // listCarUserOwns(arg1, arg2).then((res) => ...) use case
 // lists all the cars owned by a user
 const listCarUserOwns = (owner, db) =>  {
-    db.any(`SELECT c.licensePlate, c.carBrand, c.carModel FROM userOwnsACar c WHERE c.owner = ${owner}`)
+    db.any(`
+        SELECT c.licensePlate, c.carBrand, c.carModel 
+        FROM userOwnsACar c 
+        WHERE c.owner = ${owner};
+        `)
         .then(() => {
             console.log("success!");
             // success;
@@ -55,8 +75,16 @@ const listCarUserOwns = (owner, db) =>  {
 
 // Given a user, create a car ride to be advertised
 const advertiseCarRide = (user, car, date, time, origin, destination, db) =>  {
-    db.none(`INSERT INTO advertisedCarRide(driver, car, date, time, origin, destination) ' +
-        'VALUES(${user}, ${car}, ${date}, ${time}, ${origin}, ${destination}`)
+    db.none(`
+        INSERT INTO advertisedCarRide(driver, car, date, time, origin, destination) 
+        VALUES(
+            ${user}, 
+            ${car}, 
+            ${date}, 
+            ${time}, 
+            ${origin}, 
+            ${destination}
+        );`)
         .then(() => {
             console.log("success!");
             // success;
@@ -69,7 +97,9 @@ const advertiseCarRide = (user, car, date, time, origin, destination, db) =>  {
 
 // List all the advertised car rides
 const listAdvertisedCarRides = (db) =>  {
-    db.any(`SELECT a.origin, a.destination, a.date, a.time FROM advertisedCarRide a`)
+    db.any(`
+        SELECT a.origin, a.destination, a.date, a.time 
+        FROM advertisedCarRide a;`)
         .then(() => {
             console.log("success!");
             // success;
