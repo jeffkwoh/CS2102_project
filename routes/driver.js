c4a206ad3440b01b72cfd3314b277d178cc73fa5
chikchengyao <chikchengyao@gmail.com>
2018-10-12 06:00:52 +0800

    Enable user_id_field for routes/driver.js

--- routes/driver.js
+++ routes/driver.js
@@ -4,11 +4,13 @@ var db = require("../model/db.js")
 
 /* GET car rides listing page. */
 router.get('/', async function(req, res, next) {
+  const driver_id = req.query.user_id_field;
+
   // fetch:
   // 1) car rides that are pending
   // 2) car rides that are confirmed
-  const confirmedRides = await db.ride.listConfirmedRidesForDriver(1, db.exposedInstance)
-  const pendingRides = await db.ride.listPendingRidesForDriver(1, db.exposedInstance)
+  const confirmedRides = await db.ride.listConfirmedRidesForDriver(driver_id, db.exposedInstance)
+  const pendingRides = await db.ride.listPendingRidesForDriver(driver_id, db.exposedInstance)
   res.render('driver', { confirmedRides, pendingRides });
 });
 
