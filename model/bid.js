df28b71f16a210d7adc60876b1c87710c221d20b
unsatisfiedpopcorn <jefferykwoh@gmail.com>
2018-10-12 01:51:05 +0800

    Add return to all SQL promises
    - Refactor bidding.js
    - NOTE: Dangerous commit string interpolation on unsanitised input
    might cause queries to fail.

--- model/bid.js
+++ model/bid.js
@@ -1,7 +1,7 @@
 // Creates a bid for a user
 const createUserBid = (user, bidAmount, driver, date, time, origin, destination, db) =>  {
-    db.none(
-        `
+  return db.none(
+      `
         INSERT INTO bid(bidder, bidStatus, bidAmount, driver, date, time, origin, destination) 
         VALUES(
             ${user}, 
@@ -14,19 +14,19 @@ const createUserBid = (user, bidAmount, driver, date, time, origin, destination,
             ${destination}
         );
         `)
-        .then(() => {
-            console.log("success!");
-            // success;
-        })
-        .catch(error => {
-            console.log(error);
-            // error;
-        });
+  .then(() => {
+    console.log("success!");
+    // success;
+  })
+  .catch(error => {
+    console.log(error);
+    // error;
+  });
 };
 
 // Updates the bid of a User
 const updateUserBid = (user, bidAmount, driver, date, time, origin, destination, db) =>  {
-    db.none(`
+  return db.none(`
         UPDATE bid 
         SET b.bidAmount = ${bidAmount} 
         FROM bid b 
@@ -37,19 +37,19 @@ const updateUserBid = (user, bidAmount, driver, date, time, origin, destination,
         AND b.origin = ${origin}
         AND b.destination = ${destination};
         `)
-        .then(() => {
-            console.log("success!");
-            // success;
-        })
-        .catch(error => {
-            console.log(error);
-            // error;
-        });
+  .then(() => {
+    console.log("success!");
+    // success;
+  })
+  .catch(error => {
+    console.log(error);
+    // error;
+  });
 };
 
 // Deletes the bid of a User
 const deleteUserBid = (user, driver, date, time, origin, destination, db) =>  {
-    db.none(`
+  return db.none(`
         DELETE FROM bid b 
         WHERE b.bidder = ${user} 
         AND b.driver = ${driver} 
@@ -57,36 +57,37 @@ const deleteUserBid = (user, driver, date, time, origin, destination, db) =>  {
         AND b.time = ${time} 
         AND b.origin = ${origin} 
         AND b.destination = ${destination};`)
-        .then(() => {
-            console.log("success!");
-            // success;
-        })
-        .catch(error => {
-            console.log(error);
-            // error;
-        });
+  .then(() => {
+    console.log("success!");
+    // success;
+  })
+  .catch(error => {
+    console.log(error);
+    // error;
+  });
 };
 
 // List all bids a user has made
 const listBidsAUserHas = (user, db) =>  {
-    db.any(`
+  return db.any(`
         SELECT b.date, b.time, b.origin, b.destination, b.bidAmount 
         FROM bid b WHERE b.bidder = ${user};`)
-        .then(() => {
-            console.log("success!");
-            // success;
-        })
-        .catch(error => {
-            console.log(error);
-            // error;
-        });
+  .then((result) => {
+    console.log(`List bids success:\n${result}`);
+    // success;
+    return result;
+  })
+  .catch(error => {
+    console.log(error);
+    // error;
+  });
 };
 
 // Updates an existing bid in the table Bid as successful
 const setBidAsSuccessful = (successfulBidder, driver, date, time, origin, destination, db) =>  {
-    db.none(`
+  return db.none(`
         UPDATE bid 
-        SET b.bidStatus = ${successful} 
+        SET b.bidStatus = 'successful' 
         FROM bid b 
         WHERE b.bidder = ${successfulBidder} 
         AND b.driver = ${driver} 
@@ -94,19 +95,19 @@ const setBidAsSuccessful = (successfulBidder, driver, date, time, origin, destin
         AND b.time = ${time} 
         AND b.origin = ${origin} 
         AND b.destination = ${destination};`)
-        .then(() => {
-            console.log("success!");
-            // success;
-        })
-        .catch(error => {
-            console.log(error);
-            // error;
-        });
+  .then(() => {
+    console.log("success!");
+    // success;
+  })
+  .catch(error => {
+    console.log(error);
+    // error;
+  });
 };
 
 // Updates an existing bid in the table Bid as unsuccessful
 const setBidAsUnsuccessful = (unsuccessfulBidder, driver, date, time, origin, destination, db) =>  {
-    db.none(`
+  return db.none(`
         UPDATE bid 
         SET b.bidStatus = 'unsuccessful' 
         FROM bid b 
@@ -116,21 +117,21 @@ const setBidAsUnsuccessful = (unsuccessfulBidder, driver, date, time, origin, de
         AND b.time = ${time} 
         AND b.origin = ${origin}
         AND b.destination = ${destination};`)
-        .then(() => {
-            console.log("success!");
-            // success;
-        })
-        .catch(error => {
-            console.log(error);
-            // error;
-        });
+  .then(() => {
+    console.log("success!");
+    // success;
+  })
+  .catch(error => {
+    console.log(error);
+    // error;
+  });
 };
 
 module.exports = {
-    createUserBid,
-    updateUserBid,
-    listBidsAUserHas,
-    deleteUserBid,
-    setBidAsSuccessful,
-    setBidAsUnsuccessful
-}
+  createUserBid,
+  updateUserBid,
+  listBidsAUserHas,
+  deleteUserBid,
+  setBidAsSuccessful,
+  setBidAsUnsuccessful
+};
--- model/user.js
+++ model/user.js
@@ -1,6 +1,6 @@
 // Allows creation of usr account
 const createUserAppAccount = (userEmail, userContactNumber, userName, userPassword, db) =>  {
-    return db.none(`
+  return db.none(`
         INSERT INTO appUserAccount(email, contactNumber, name, password) 
         VALUES(
             ${userEmail}, 
@@ -8,14 +8,14 @@ const createUserAppAccount = (userEmail, userContactNumber, userName, userPasswo
             ${userName}, 
             ${userPassword}
         );`)
-        .then(() => {
-            console.log("success!");
-            // success;
-        })
-        .catch(error => {
-            console.log(error);
-            // error;
-        });
+  .then(() => {
+    console.log("Create user success!");
+    // success;
+  })
+  .catch(error => {
+    console.log(error);
+    // error;
+  });
 };
 
 // Lists out the names and emails of everyone in userAppAccounts
@@ -23,20 +23,20 @@ const listUserAppAccount = (db) =>  {
   return db.any(`
         SELECT a.name, a.email 
         FROM appUserAccount a;`)
-        .then((result) => {
-            console.log("success!");
-            // success;
-            return result;
-        })
-        .catch(error => {
-            console.log(error);
-            // error;
-        });
+  .then((result) => {
+    console.log(`List user success:\n ${result}`);
+    // success;
+    return result;
+  })
+  .catch(error => {
+    console.log(error);
+    // error;
+  });
 };
 
 // Adds a car to a specific user
 const addCarToUser = (owner, licensePlate, carBrand, carModel, numSeats, db) =>  {
-    return db.none(`
+  return db.none(`
         INSERT INTO userOwnsACar(licensePlate, owner, carBrand, carModel, numSeats) 
         VALUES(
             ${licensePlate}, 
@@ -45,39 +45,39 @@ const addCarToUser = (owner, licensePlate, carBrand, carModel, numSeats, db) => 
             ${carModel}, 
             ${numSeats}
         );`
-        )
-        .then(() => {
-            console.log("success!");
-            // success;
-        })
-        .catch(error => {
-            console.log(error);
-            // error;
-        });
+  )
+  .then(() => {
+    console.log("Add car success!");
+    // success;
+  })
+  .catch(error => {
+    console.log(error);
+    // error;
+  });
 };
 
 // listCarUserOwns(arg1, arg2).then((res) => ...) use case
 // lists all the cars owned by a user
 const listCarUserOwns = (owner, db) =>  {
-    return db.any(`
+  return db.any(`
         SELECT c.licensePlate, c.carBrand, c.carModel 
         FROM userOwnsACar c 
         WHERE c.owner = ${owner};
         `)
-        .then((result) => {
-            console.log("success!");
-            // success;
-            return result;
-        })
-        .catch(error => {
-            console.log(error);
-            // error;
-        });
+  .then((result) => {
+    console.log(`List car success\n:${result}`);
+    // success;
+    return result;
+  })
+  .catch(error => {
+    console.log(error);
+    // error;
+  });
 };
 
 // Given a user, create a car ride to be advertised
 const advertiseCarRide = (user, car, date, time, origin, destination, db) =>  {
-    return db.none(`
+  return db.none(`
         INSERT INTO advertisedCarRide(driver, car, date, time, origin, destination) 
         VALUES(
             ${user}, 
@@ -87,37 +87,37 @@ const advertiseCarRide = (user, car, date, time, origin, destination, db) =>  {
             ${origin}, 
             ${destination}
         );`)
-        .then(() => {
-            console.log("success!");
-            // success;
-        })
-        .catch(error => {
-            console.log(error);
-            // error;
-        });
+  .then(() => {
+    console.log("Advertise car ride success!");
+    // success;
+  })
+  .catch(error => {
+    console.log(error);
+    // error;
+  });
 };
 
 // List all the advertised car rides
 const listAdvertisedCarRides = (db) =>  {
-    return db.any(`
+  return db.any(`
         SELECT a.origin, a.destination, a.date, a.time 
         FROM advertisedCarRide a;`)
-        .then((result) => {
-            console.log("success!");
-            // success;
-            return result
-        })
-        .catch(error => {
-            console.log(error);
-            // error;
-        });
+  .then((result) => {
+    console.log(`List advertised car rides success:\n${result}`);
+    // success;
+    return result
+  })
+  .catch(error => {
+    console.log(error);
+    // error;
+  });
 };
 
 module.exports = {
-    createUserAppAccount,
-    listUserAppAccount,
-    addCarToUser,
-    listCarUserOwns,
-    advertiseCarRide,
-    listAdvertisedCarRides
+  createUserAppAccount,
+  listUserAppAccount,
+  addCarToUser,
+  listCarUserOwns,
+  advertiseCarRide,
+  listAdvertisedCarRides
 };
