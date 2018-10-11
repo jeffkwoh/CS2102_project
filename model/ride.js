cd2eea7beb05c7135ea80cb90c68b7135fc5c540
remo5000 <vigneshshankar@gmail.com>
2018-10-12 01:50:30 +0800

    Add return for listAdvertisedCarRides

--- model/ride.js
+++ model/ride.js
@@ -1,32 +1,33 @@
-/**
- * Given a user, create a car ride to be advertised
- */
-const advertiseCarRide = (user, car, date, time, origin, destination, db) =>  {
-    db.none(`
+**
+* Given a user, create a car ride to be advertised
+  */
+  const advertiseCarRide = (user, car, date, time, origin, destination, db) =>  {
+    return db.none(`
       INSERT INTO advertisedCarRide(driver, car, date, time, origin, destination)
-        VALUES(${user}, ${car}, ${date}, ${time}, ${origin}, ${destination});`)
-        .then(() => {
-            console.log(`Created car ride by User ${user}, on ${date}, ${time}!`);
-        })
-        .catch(error => {
-            console.log(error);
-        });
-};
+      VALUES(${user}, ${car}, ${date}, ${time}, ${origin}, ${destination});`)
+      .then(() => {
+        console.log(`Created car ride by User ${user}, on ${date}, ${time}!`);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
 /**
  * List all advertised car rides, that are upcoming.
  */
 const listAdvertisedCarRides = (db) =>  {
-    db.all('SELECT a.origin, a.destination, a.date, a.time FROM advertisedCarRide a;')
-        .then(() => {
-            console.log(`Retrived all upcoming car rides!`);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+  return db.all('SELECT a.origin, a.destination, a.date, a.time FROM advertisedCarRide a;')
+    .then((result) => {
+      console.log(`Retrived all upcoming car rides!`)
+      return result
+    })
+    .catch(error => {
+      console.log(error);
+    });
 };
 
 module.exports = {
-    advertiseCarRide,
-    listAdvertisedCarRides,
+  advertiseCarRide,
+  listAdvertisedCarRides,
 };
