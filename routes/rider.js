974367a9809767b51bc74a14243a596569dc11cb
remo5000 <vigneshshankar@gmail.com>
2018-10-12 03:37:57 +0800

    Rename properties for car ride

--- routes/rider.js
+++ routes/rider.js
@@ -9,21 +9,22 @@ router.get('/', async function(req, res, next) {
   // 2) bids (for car rides) that are pending
   // 3) All upcoming car rides that can be bid for
   const confirmedRides = [
-    { id: 0, date: "2018-10-30", time: "1500", startLocation:"Suntec City", endLocation:"NUS" }
+    { id: 0, date: "2018-10-30", time: "1500", origin:"Suntec City", destination:"NUS" }
   ]
   const biddedRides = [
-    { id: 4, date: "2018-10-30", time: "1500", startLocation:"Ivan City", endLocation:"NUS", bid:34.2 },
-    { id: 5, date: "2018-10-30", time: "1500", startLocation:"Ivan City", endLocation:"NUS", bid:34.2 },
-    { id: 6, date: "2018-10-30", time: "1500", startLocation:"Ivan City", endLocation:"NUS", bid:34.2 },
-    { id: 7, date: "2018-10-30", time: "1500", startLocation:"Ivan City", endLocation:"NUS", bid:34.2 },
+    { id: 4, date: "2018-10-30", time: "1500", origin:"Ivan City", destination:"NUS", bid:34.2 },
+    { id: 5, date: "2018-10-30", time: "1500", origin:"Ivan City", destination:"NUS", bid:34.2 },
+    { id: 6, date: "2018-10-30", time: "1500", origin:"Ivan City", destination:"NUS", bid:34.2 },
+    { id: 7, date: "2018-10-30", time: "1500", origin:"Ivan City", destination:"NUS", bid:34.2 },
   ]
   const availableRides = [
-    { id: 1, date: "2018-10-30", time: "1500", startLocation:"Suntec City", endLocation:"NUS" },
-    { id: 2, date: "2018-10-30", time: "1500", startLocation:"Suntec City", endLocation:"NUS" },
-    { id: 3, date: "2018-10-30", time: "1500", startLocation:"Suntec City", endLocation:"NUS" },
+    { id: 1, date: "2018-10-30", time: "1500", origin:"Suntec City", destination:"NUS" },
+    { id: 2, date: "2018-10-30", time: "1500", origin:"Suntec City", destination:"NUS" },
+    { id: 3, date: "2018-10-30", time: "1500", origin:"Suntec City", destination:"NUS" },
   ]
 
   const rides = await db.ride.listAdvertisedCarRides(db.exposedInstance)
+  console.log(rides)
   res.render('rider', { confirmedRides, biddedRides, availableRides });
 });
 
