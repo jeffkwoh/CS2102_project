05610cdad7cb453cd49d1df6b1d191e85c27fdf4
chikchengyao <chikchengyao@gmail.com>
2018-10-12 02:44:27 +0800

    Fix user.js interpolation

--- model/user.js
+++ model/user.js
@@ -3,19 +3,19 @@ const createUserAppAccount = (userEmail, userContactNumber, userName, userPasswo
   return db.none(`
         INSERT INTO appUserAccount(email, contactNumber, name, password) 
         VALUES(
-            ${userEmail}, 
-            ${userContactNumber}, 
-            ${userName}, 
-            ${userPassword}
-        );`)
-  .then(() => {
-    console.log("Create user success!");
-    // success;
-  })
-  .catch(error => {
-    console.log(error);
-    // error;
-  });
+            $1, 
+            $2, 
+            $3, 
+            $4
+        );`, [userEmail, userContactNumber, userName, userPassword])
+        .then(() => {
+            console.log("Create user success!");
+            // success;
+        })
+        .catch(error => {
+            console.log(error);
+            // error;
+        });
 };
 
 // Lists out the names and emails of everyone in userAppAccounts
@@ -39,40 +39,39 @@ const addCarToUser = (owner, licensePlate, carBrand, carModel, numSeats, db) => 
   return db.none(`
         INSERT INTO userOwnsACar(licensePlate, owner, carBrand, carModel, numSeats) 
         VALUES(
-            ${licensePlate}, 
-            ${owner}, 
-            ${carBrand}, 
-            ${carModel}, 
-            ${numSeats}
-        );`
-  )
-  .then(() => {
-    console.log("Add car success!");
-    // success;
-  })
-  .catch(error => {
-    console.log(error);
-    // error;
-  });
+            $1, 
+            $2, 
+            $3, 
+            $4, 
+            $5
+        );`, [licensePlate, owner, carBrand, carModel, numSeats])
+        .then(() => {
+            console.log("Add car success!");
+            // success;
+        })
+        .catch(error => {
+            console.log(error);
+            // error;
+        });
 };
 
 // listCarUserOwns(arg1, arg2).then((res) => ...) use case
 // lists all the cars owned by a user
 const listCarUserOwns = (owner, db) =>  {
   return db.any(`
-        SELECT c.licensePlate, c.carBrand, c.carModel 
-        FROM userOwnsACar c 
-        WHERE c.owner = ${owner};
-        `)
-  .then((result) => {
-    console.log(`List car success\n:${result}`);
-    // success;
-    return result;
-  })
-  .catch(error => {
-    console.log(error);
-    // error;
-  });
+      SELECT c.licensePlate, c.carBrand, c.carModel 
+      FROM userOwnsACar c 
+      WHERE c.owner = $1;
+    `, [owner])
+    .then((result) => {
+      console.log(`List car success\n:${result}`);
+      // success;
+      return result;
+    })
+    .catch(error => {
+      console.log(error);
+      // error;
+    });
 };
 
 // Given a user, create a car ride to be advertised
@@ -80,21 +79,21 @@ const advertiseCarRide = (user, car, date, time, origin, destination, db) =>  {
   return db.none(`
         INSERT INTO advertisedCarRide(driver, car, date, time, origin, destination) 
         VALUES(
-            ${user}, 
-            ${car}, 
-            ${date}, 
-            ${time}, 
-            ${origin}, 
-            ${destination}
-        );`)
-  .then(() => {
-    console.log("Advertise car ride success!");
-    // success;
-  })
-  .catch(error => {
-    console.log(error);
-    // error;
-  });
+            $1, 
+            $2, 
+            $3, 
+            $4, 
+            $5, 
+            $6
+        );`, [user, car, date, time, origin, destination])
+      .then(() => {
+        console.log("Advertise car ride success!");
+        // success;
+      })
+      .catch(error => {
+        console.log(error);
+        // error;
+      });
 };
 
 // List all the advertised car rides
