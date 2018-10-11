4d22f16e3d401f142626db0b77a033a6345ea5f8
unsatisfiedpopcorn <jefferykwoh@gmail.com>
2018-10-12 00:52:33 +0800

    Fix missing return in model/user

--- routes/users.js
+++ routes/users.js
@@ -3,21 +3,19 @@ var router = express.Router();
 var db = require("../model/db.js");
 
 /* GET users listing page. */
-router.get('/', function(req, res, next) {
-  const mockUsers = [
-    { name: "alison" },
-    { name: "burgers" },
-    { name: "constantine" },
-    { name: "diplo" },
-  ]
-  res.render('users', { users: mockUsers });
+router.get('/', async function(req, res, next) {
+  const users = await db.user
+  .listUserAppAccount(db.exposedInstance);
+
+  res.render('users', { users: users });
 });
 
 /* POST user creation. */
 router.post('/create', function(req, res, next) {
-  const user_name = req.body.name_field
+  const user_name = req.body.name_field;
   // add user to db here
+
   res.redirect('/users')
 });
 
-module.exports = router
+module.exports = router;
