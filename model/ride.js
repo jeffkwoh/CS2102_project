9494d129da898d03a2ef16f87fd5530cde3846d2
Evan Mok <31198642+evanmok2401@users.noreply.github.com>
2018-11-04 19:55:32 +0800

    b.bidStatus = 'pending'.
    Changed from b.bidStatus LIKE %pending%

--- model/ride.js
+++ model/ride.js
@@ -147,7 +147,7 @@ const listPendingRidesForDriver = async (user, db) => {
       AND a.time = b.time
       AND a.origin = b.origin
       AND a.destination = b.destination
-    WHERE a.driver = $1 AND (b.bidStatus LIKE '%pending%' OR b.bidStatus IS NULL)
+    WHERE a.driver = $1 AND (b.bidStatus = 'pending' OR b.bidStatus IS NULL)
     GROUP BY a.driver, a.date, a.time, a.origin, a.destination;
     `,
       [user]
