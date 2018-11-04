782148d52987389cf288ad74826197cfc545f081
remo5000 <vigneshshankar@gmail.com>
2018-11-04 17:48:21 +0800

    Use filters to call db functions

--- routes/rider.js
+++ routes/rider.js
@@ -6,20 +6,32 @@ const connect = require('connect-ensure-login')
 /* GET user-rider's rides/bids listing page. */
 router.get('/', connect.ensureLoggedIn('/login') ,async function(req, res, next) {
   const riderId = req.user
+  const filters = {
+    driverFilter: req.body.driverFilter,
+    dateFilter: req.body.dateFilter,
+    timeFilter: req.body.timeFilter,
+    originFilter: req.body.originFilter,
+    destinationFilter: req.body.destinationFilter,
+    bidAmountFilter: req.body.bidAmountFilter,
+    bidStatusFilter: req.body.bidStatusFilter
+  }
   // fetch:
   // 1) car rides that are confirmed for this rider
   // 2) bids (for car rides) that are pending
   // 3) All upcoming car rides that can be bid for
   const confirmedRides = await db.ride.listConfirmedRidesForRider(
     riderId,
+    filters,
     db.exposedInstance
   )
   const biddedRides = await db.bid.listPendingBidsForUser(
     riderId,
+    filters,
     db.exposedInstance
   )
   const availableRides = await db.ride.listAvailableAdvertisedCarRidesForRider(
     riderId,
+    filters,
     db.exposedInstance
   )
   res.render('rider', { riderId, confirmedRides, biddedRides, availableRides })
